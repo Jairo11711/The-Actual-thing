3f7ba8dd075d7149c6c1f565668be92e56a1ac84
Jairo11711 <116799263+Jairo11711@users.noreply.github.com>
2026-04-10 20:13:51 +0800

    Decoupled the main css files into several smaller css files. As well as established the  component system for  templates.
    -stylesheet.css is now extracted into several smaller css files based on function and is the impoeted into the main.css
    -Rework the main templates into a inheritance structure and composited with components.

--- test-server/accounts/urls.py
+++ test-server/accounts/urls.py
@@ -4,5 +4,6 @@ from .import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('registration', views.registration, name='registration'),
-    path('logging', views.logging, name='logging')
+    path('login', views.login_user, name='login'),
+    path('logout', views.logout_user, name='logout')
 ]
--- test-server/accounts/views.py
+++ test-server/accounts/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 # Create your views here.
 def index(request):
@@ -8,5 +11,8 @@ def registration(request):
     return render(request, 'accounts/registration_form.html')
 
 
-def logging(request):
-    return render(request, 'accounts/logging_form.html')
+def login_user(request):
+    return render(request, 'accounts/login_form.html')
+
+def logout_user(request):
+    return
