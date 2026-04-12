e2844e7f0d5406d24f7a968c02454a01f4ad1c74
Jairo11711 <116799263+Jairo11711@users.noreply.github.com>
2026-04-12 11:43:16 +0800

    Added the ability to log in and log out
    This does NOT the notification of success and failure of login.

--- test-server/accounts/views.py
+++ test-server/accounts/views.py
@@ -1,18 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 
+
 # Create your views here.
 def index(request):
-    return render(request, 'accounts/base.html')
+    return render(request, 'accounts/base_accounts.html')
 
 def registration(request):
     return render(request, 'accounts/registration_form.html')
 
 
 def login_user(request):
-    return render(request, 'accounts/login_form.html')
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Succesfully Logged In")
+            return redirect('main:index')
+        else:
+            messages.success(request, "Error: Please Try Again")
+            return redirect('accounts:login')
+        
+    else: 
+        return render(request, 'accounts/login_form.html', {})
 
 def logout_user(request):
-    return
+    logout(request)
+    return redirect('main:index')
